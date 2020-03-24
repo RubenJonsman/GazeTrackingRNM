6af0a48f45f0439eaa75b729191ba983c415cd7e
ruben <rubenjonsman@gmail.com>
2020-03-24 12:53:17 +0100

    j

---
+++ reinholdgaze.py
@@ -0,0 +1,42 @@
+import cv2
+from gaze_tracking import GazeTracking
+
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+sentence = []
+
+while True:
+    # We get a new frame from the webcam
+    _, frame = webcam.read()
+
+    # We send this frame to GazeTracking to analyze it
+    gaze.refresh(frame)
+
+    frame = gaze.annotated_frame()
+    text = ""
+    alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å']
+
+    if gaze.is_blinking():
+        text = "Blinking"
+    elif gaze.is_right():
+        text = "Looking right"
+        k = round(len(alfa) / 2)
+        print(alfa[k:])
+    elif gaze.is_left():
+        text = "Looking left"
+    elif gaze.is_center():
+        text = "Looking center"
+
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+    left_pupil = gaze.pupil_left_coords()
+    right_pupil = gaze.pupil_right_coords()
+    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+
+    cv2.imshow("Demo", frame)
+    if cv2.waitKey(1) == 27:
+        break
